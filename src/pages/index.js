5d39e25ee1076a5703acf61b3901caf883800afe
Philippe35507 <55062877+Philippe35507@users.noreply.github.com>
2020-01-11 21:37:31 +0000

    Ajout Head Page + modif page 404

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
+import Head from "../components/head"
 
 
 import Layout from "../components/layout"
@@ -7,6 +8,7 @@ import Layout from "../components/layout"
 const IndexPage = () => {
   return (
     <Layout>
+          <Head title="home" />
           <h1>Hello !!</h1>
           <h2>I am Philippe</h2>
           <p>Need help ? <Link to="/contact">CONTACT ME</Link></p>
